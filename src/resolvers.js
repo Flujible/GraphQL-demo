95bdc096b71dbc8ba960a79cd8e70adb6d4e56ac
George Bryant <george.bryant@mirumagency.com>
2018-11-10 16:47:28 +0000

    Define an endpoint for generating a playing card

---
+++ src/resolvers.js
@@ -0,0 +1,37 @@
+const resolvers = {
+    Query: {
+      drawCard: (obj, args) => {        
+        let value = args.value ? args.value : genValue();
+        let suit = args.suit ? args.suit : genSuit();
+
+        return value + suit;
+      },
+    },
+  };
+   
+const genValue = () => {
+  const value = Math.floor(Math.random()*13 + 1);
+
+  const mapping = {
+    '1': 'A',
+    '10':'0',
+    '11': 'J',
+    '12': 'Q',
+    '13': 'K',
+  }
+
+  return value >= 10 || value === 1 ? mapping[value] : value.toString();
+}
+
+const genSuit = () => {
+  const value = Math.floor(Math.random()*4 +1);
+  const mapping = {
+    '1': 'H',
+    '2': 'S',
+    '3': 'C',
+    '4': 'D'
+  }
+  return mapping[value];
+}
+
+  export default resolvers;
